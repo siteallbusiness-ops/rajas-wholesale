fd6c13b4f8b9194b7e53842f8cfb4eda851c403d
siteallbusiness-ops <siteallbusiness@gmail.com>
2026-06-27 13:52:34 +0530

    add favicon

--- app/layout.js
+++ app/layout.js
@@ -2,10 +2,20 @@ import SiteLayout from "@/components/Layout/SiteLayout";
 import { createMetadata } from "@/utils/metadata";
 import "./globals.css";
 
-export const metadata = createMetadata({
-  title: "",
-  description: "Rajas Wholesale - Bringing bulk to you. Confectionery, drinks, snacks and more.",
-});
+export const metadata = {
+  ...createMetadata({
+    title: "",
+    description:
+      "Rajas Wholesale - Bringing bulk to you. Confectionery, drinks, snacks and more.",
+  }),
+  manifest: "/site.webmanifest",
+  themeColor: "#c41e3a",
+  appleWebApp: {
+    capable: true,
+    title: "Rajas Wholesale",
+    statusBarStyle: "default",
+  },
+};
 
 export default function RootLayout({ children }) {
   return (
